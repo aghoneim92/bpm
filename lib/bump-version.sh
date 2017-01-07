d6a22bd27f59019fef2050c7134a7147e9b811a8
Ahmed Ghoneim <ahmedghoneim92@gmail.com>
2017-01-07 18:41:30 +0100

    1.0.10

--- lib/bump-version.sh
+++ lib/bump-version.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
-node lib/bump-version $1
+git branch -D bump-version
 git checkout -b bump-version
+node lib/bump-version $1
 git add --all
 git commit -m $1
 git tag -a $1 -m $1 --force
